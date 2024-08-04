5e7e784e045ba3af23e2de9d9da8478758111d8e
inogii <inigogvvallejo@gmail.com>
2024-08-04 22:46:58 +0200

    audioldm experiment setup script

---
+++ audioldm.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/inogii/AudioLDM-training-finetuning
+cd AudioLDM-training-finetuning
+bash <(curl -sSL https://g.bodaay.io/hfd) -h
+chmod +x hfdownloader
+./hfdownloader -d inogii/audiocaps_compressed -c 10 -s /root/AudioLDM-training-finetuning/data/dataset
+python merge_chunks.py
+rm -rf data/dataset/inogii_audiocaps_compressed
+unzip data/dataset/reassembled_dataset.zip
